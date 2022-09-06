c1b08b223fbe6c40b8f8739050bd81ded3fe1d89
Hugo Salles <salles.hugo05@gmail.com>
2022-09-06 17:54:30 -0400

    adding more functionilty for database timestamp

--- functions/index.js
+++ functions/index.js
@@ -1,49 +1,49 @@
-import functions from 'firebase-functions'
-import express from 'express';
-import cors from 'cors';
-import { MongoClient } from 'mongodb';
-import 'dotenv/config'
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
 
+const client = new MongoClient(process.env.MONGO_URI);
+const database = client.db("MechConnectDB");
+const MechConnectdbc = database.collection("MechConnectdbc");
 
+client.connect();
+console.log("Mongo connected");
 
-const client = new MongoClient(process.env.MONGO_URI)
-const database = client.db('MechConnectDB')
-const MechConnectdbc = database.collection('MechConnectdbc')
-
-client.connect()
-console.log('Mongo connected')
-
-
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-
 // GET
-app.get('/work', async (req, res) => {
-    const allVehicles = await MechConnectdbc.find().toArray()
-    res.send(allVehicles)
-})
+app.get("/vehicles", async (req, res) => {
+  const allVehicles = await MechConnectdbc.find().toArray(req.body);
+  res.send(allVehicles);
+});
 
-// POST 
-app.post('/', async (req, res) => {
-    await MechConnectdbc.insertOne(req.body)
-    
-    res.send('not work')
-})
+// POST
+app.post("/newVehicle", async (req, res) => {
+  const newVehicle = {
+    info: req.body,
+    createdAt: new Date(),
+  };
+  await MechConnectdbc.insertOne(newVehicle);
+
+  res.send("not work");
+});
 
 // DELETE
 
-app.delete('/', async (req, res) => {
-    await MechConnectdbc.findOneAndDelete(req.query)
-    res.send('item deleted')
-})
+app.delete("/", async (req, res) => {
+  await MechConnectdbc.findOneAndDelete(req.query);
+  res.send("item deleted");
+});
 
-// PUT  
+// PUT
 
-app.put('/', (req, res) => {
-    MechConnectdbc.findOneAndUpdate(req.query, {$set: req.body})
-})
+app.put("/", (req, res) => {
+  MechConnectdbc.findOneAndUpdate(req.query, { $set: req.body });
+});
 
 export const api = functions.https.onRequest(app);
 
